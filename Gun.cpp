8dc4a1d9d75d2ce1b87c6690cfa5df4132b1e7d3
Delta0077 <adas0262@gmail.com>
2021-03-24 01:54:47 +0530

    LineTraceSingleByChannel with FHitResult out parameter

--- Gun.cpp
+++ Gun.cpp
@@ -36,8 +36,19 @@ void AGun::PullTrigger()
 
 	OwnerController->GetPlayerViewPoint(Location, Rotation);
 	
+	FVector End = Location + Rotation.Vector() * MaxRange;
+
 	
-	DrawDebugCamera(GetWorld(), Location, Rotation, 90, 2 , FColor::Red, true);
+
+
+	FHitResult Hit;
+	bool bSuccess = GetWorld()->LineTraceSingleByChannel(Hit, Location, End, ECollisionChannel::ECC_GameTraceChannel1);
+
+	if(bSuccess)
+	{
+	DrawDebugPoint(GetWorld(), Hit.Location, 20, FColor::Green, true);
+	}
+	//DrawDebugCamera(GetWorld(), Location, Rotation, 90, 2 , FColor::Red, true);
 }
 
 // Called when the game starts or when spawned
