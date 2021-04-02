c34d9c9239d46e48449ae7cacc89a56c8ff091a2
Delta0077 <adas0262@gmail.com>
2021-04-02 22:07:40 +0530

    Update ShooterAIController.cpp

--- ShooterAIController.cpp
+++ ShooterAIController.cpp
@@ -25,16 +25,16 @@ void AShooterAIController::Tick(float DeltaTime)
 {
     Super::Tick(DeltaTime);
    
-    APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+    // APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
 
-    if(LineOfSightTo(PlayerPawn))
-    {
-        GetBlackboardComponent()->SetValueAsVector(TEXT("PlayerLocation"), PlayerPawn->GetActorLocation());
-        GetBlackboardComponent()->SetValueAsVector(TEXT("LastKnownPlayerLocation"), PlayerPawn->GetActorLocation());
-    }
-    else
-    {
-        GetBlackboardComponent()->ClearValue(TEXT("PlayerLocation"));
-    }
+    // if(LineOfSightTo(PlayerPawn))
+    // {
+    //     GetBlackboardComponent()->SetValueAsVector(TEXT("PlayerLocation"), PlayerPawn->GetActorLocation());
+    //     GetBlackboardComponent()->SetValueAsVector(TEXT("LastKnownPlayerLocation"), PlayerPawn->GetActorLocation());
+    // }
+    // else
+    // {
+    //     GetBlackboardComponent()->ClearValue(TEXT("PlayerLocation"));
+    // }
 
 }
